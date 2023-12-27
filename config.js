6ae2d4ca69c5e13519ebfa53dce85b03fb1e4749
Der Dunkle Ritter <128868868+derdunkleritter@users.noreply.github.com>
2023-12-27 16:06:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254110077535', 'zamzack254', true],
-  ['254110077535', 'kinflux digital', true], 
+  ['48729759616', '𝐌𝐀𝐒𝐓𝐄𝐑 𝐆𝐑𝐀𝐕𝐈𝐓𝐘', true],
+  ['254114518934', 'kinflux digital', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254110077535','254110377776'] 
-global.prems = ['254110077535', '254110377776', '254765777423']
+global.mods = ['48729759616','48729759616'] 
+global.prems = ['48729759616','48729759616','48729759616']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'kinflux bot'
+global.botname = '𝐆𝐑𝐀𝐕𝐈𝐓𝐘'𝐒 🧘🏻‍♂️𝐁𝐎𝐓🖲️'
 global.premium = 'true'
-global.packname = 'kinflux┃ᴮᴼᵀ' 
-global.author = 'zamzack254' 
+global.packname = '𝐆𝐑𝐀𝐕𝐈𝐓𝐘'𝐒 🧘🏻‍♂️𝐁𝐎𝐓🖲️' 
+global.author = '𝐌𝐀𝐒𝐓𝐄𝐑 𝐆𝐑𝐀𝐕𝐈𝐓𝐘' 
 global.igfg = '▢ Follow on Instagram\nhttps://chat.whatsapp.com/KggVmnH7ki6HoyD0025gDD\n' 
 global.dygp = 'https://chat.whatsapp.com/KggVmnH7ki6HoyD0025gDD'
 global.fgsc = 'https://github.com/diggilly/kinflux-bot' 
